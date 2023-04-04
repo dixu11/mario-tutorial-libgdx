a1a1c08ad384b1c98d2b4ac82ce8b7f571570a10
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-04-04 09:25:40 +0200

    add box3D bodies and render them. ugly tutorial code :x

--- core/src/dixu/mario/PlayScreen.java
+++ core/src/dixu/mario/PlayScreen.java
@@ -4,20 +4,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PlayScreen implements Screen {
-    private MarioGame game;
-    private OrthographicCamera camera;
-    private Viewport viewport;
-    private Hud hud;
-    private TmxMapLoader mapLoader;
-    private TiledMap map;
-    private OrthogonalTiledMapRenderer renderer;
+    private final MarioGame game;
+    private final OrthographicCamera camera;
+    private final Viewport viewport;
+    private final Hud hud;
+    private final TmxMapLoader mapLoader;
+    private final TiledMap map;
+    private final OrthogonalTiledMapRenderer renderer;
+    private Box2DDebugRenderer boxRenderer;
+    private World world;
 
     public PlayScreen(MarioGame game) {
         this.game = game;
@@ -35,7 +43,68 @@ public class PlayScreen implements Screen {
         map = mapLoader.load("map.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         camera.position.set(viewport.getWorldWidth() / 2f, viewport.getWorldHeight() / 2f, 0);
+
+        world = new World(new Vector2(0, 0), true);
+        boxRenderer = new Box2DDebugRenderer();
+
+
+        //kinematic dont react to velocity
+        //ground
+        for (RectangleMapObject mapObject : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rectangle = mapObject.getRectangle();
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rectangle.x + rectangle.width / 2, rectangle.y + rectangle.height / 2);
+            Body body = world.createBody(bodyDef);
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(rectangle.width/2,rectangle.height/2);
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            body.createFixture(fixtureDef);
+        }
+        //pipes
+        for (RectangleMapObject mapObject : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rectangle = mapObject.getRectangle();
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rectangle.x + rectangle.width / 2, rectangle.y + rectangle.height / 2);
+            Body body = world.createBody(bodyDef);
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(rectangle.width/2,rectangle.height/2);
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            body.createFixture(fixtureDef);
+        }
+        //coins
+        for (RectangleMapObject mapObject : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rectangle = mapObject.getRectangle();
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rectangle.x + rectangle.width / 2, rectangle.y + rectangle.height / 2);
+            Body body = world.createBody(bodyDef);
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(rectangle.width/2,rectangle.height/2);
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            body.createFixture(fixtureDef);
+        }
+        //bricks
+        for (RectangleMapObject mapObject : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rectangle = mapObject.getRectangle();
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rectangle.x + rectangle.width / 2, rectangle.y + rectangle.height / 2);
+            Body body = world.createBody(bodyDef);
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(rectangle.width/2,rectangle.height/2);
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            body.createFixture(fixtureDef);
+        }
+
+
     }
+
     @Override
     public void show() {
 
@@ -45,6 +114,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         update(delta);
         renderer.render();
+        boxRenderer.render(world, camera.combined);
         SpriteBatch batch = game.getBath();
         //move to render method
         batch.setProjectionMatrix(hud.getStage().getCamera().combined); // ?
